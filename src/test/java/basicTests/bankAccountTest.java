c438996369fda88f58827a262be598a9d683f95d
Sundaram Sharma <sundaramsharma1209@hotmail.com>
2022-04-19 20:37:59 -0400

    arrestNotEquals

--- src/test/java/basicTests/bankAccountTest.java
+++ src/test/java/basicTests/bankAccountTest.java
@@ -1,12 +1,14 @@
 package basicTests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@DisplayName("Testing Bank Account")
 public class bankAccountTest {
 
     @Test
+    @DisplayName("Withdraw 300 successfully") //custom Display Name for tests
     public void testWithdraw(){
         bankAccount BankAccount = new bankAccount(500,-1000); //creating instance
         BankAccount.withdraw(300);
@@ -15,10 +17,19 @@ public class bankAccountTest {
     }
 
     @Test
+    @DisplayName("Deposit 400 successfully")
     public void testDeposit(){
         bankAccount BankAccount = new bankAccount(400,-1000); //creating instance
         BankAccount.deposit(400);
         assertEquals(800,BankAccount.getBalance());
+    }
+
+    @Test
+    @DisplayName("Withdraw will become negative")
+    public void testWithdrawNotStuckAtZero(){
+        bankAccount BankAccount = new bankAccount(500,-1000); //creating instance
+        BankAccount.withdraw(900);
+        assertNotEquals(0,BankAccount.getBalance());//compare two to check if they are not equal
     }
 
 }
