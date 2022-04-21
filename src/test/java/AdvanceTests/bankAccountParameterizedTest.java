46e92c3a4821dc1fb1522d069db5e3074e65ce51
Sundaram Sharma <sundaramsharma1209@hotmail.com>
2022-04-21 01:50:00 -0400

    Parameterized Test complete with 1 error in last

--- src/test/java/AdvanceTests/bankAccountParameterizedTest.java
+++ src/test/java/AdvanceTests/bankAccountParameterizedTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -31,5 +32,22 @@ public class bankAccountParameterizedTest {
     }
 
     @ParameterizedTest
-    @CsvSource
+    @CsvSource({"100, Mary", "200, Rishab","300, Shila"}) //The @CsvSource accepts an array of comma-separated values, and each array entry corresponds to a line in a CSV file.
+    public void depositAndNameTest(double amount, String name, bankAccount BankAccount){
+        BankAccount.deposit(amount);
+        BankAccount.setHolderName(name);
+        assertEquals(amount, BankAccount.getBalance()); // check the provided amount with balance
+        assertEquals(name, BankAccount.getHolderName()); //check the name with account Name
+    }
+
+    @ParameterizedTest
+    //@CsvSource({"100, Mary", "200, Rishab","300, Shila"}) //The @CsvSource accepts an array of comma-separated values, and each array entry corresponds to a line in a CSV file.
+    @CsvFileSource(resources = "res.csv", delimiter = ';')
+    public void depositAndNameTest2(double amount, String name, bankAccount BankAccount){
+        BankAccount.deposit(amount);
+        BankAccount.setHolderName(name);
+        assertEquals(amount, BankAccount.getBalance()); // check the provided amount with balance
+        assertEquals(name, BankAccount.getHolderName()); //check the name with account Name
+    }
+
 }
