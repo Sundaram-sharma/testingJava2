eb79e1fdf4a7c7c567d950bc6813c18d9393a24e
Sundaram Sharma <sundaramsharma1209@hotmail.com>
2022-04-21 01:39:31 -0400

    Parameterized Test 2

--- src/test/java/AdvanceTests/bankAccountParameterizedTest.java
+++ src/test/java/AdvanceTests/bankAccountParameterizedTest.java
@@ -4,17 +4,32 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.time.DayOfWeek;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(BankAccountParameterResolver.class)
 public class bankAccountParameterizedTest {
     @ParameterizedTest //Parameterized test is to execute the same test over and over again using different values.
     @ValueSource(ints = {100, 400, 800, 1000}) //provide the arguments for each invocation of test
-    @DisplayName("Deposit 400 successfully")
+    @DisplayName("Deposit successfully")
     public void testDepositValueSource(int amount, bankAccount BankAccount){
-        BankAccount.deposit(amount);
-        assertEquals(amount,BankAccount.getBalance());
+        BankAccount.deposit(amount); //deposit will be the amount
+        assertEquals(amount,BankAccount.getBalance()); //check the amount witht the current balance
     }
+
+    @ParameterizedTest
+    @EnumSource(value = DayOfWeek.class, names = {"Tuesday","Thursday"}) //In order to run a test with different values from an enumeration, we can use the @EnumSource annotation.
+    public void testDayOfWeek(DayOfWeek day){
+        assertTrue(day.toString().startsWith("T")); // the day should start with T
+
+    }
+
+    @ParameterizedTest
+    @CsvSource
 }
